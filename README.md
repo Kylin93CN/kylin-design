c5214a87c1c49dcd0fb800ba0442d70fb933be52
lvxiang <375020612@qq.com>
2021-07-19 21:35:43 +0800

    feat: add pkg info & remove sb's snapshot

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # kylin-design
 仿照antd，使用ts+react打造自己的组件库
 ## 使用
+### 打包
+` npm run build-ts`
+### 运行
+本地运行需要修改`src/index.tsx`文件
+`npm start`
+
+### 本地启动storybook
+`npm run storybook`
+
 
 ```javascript
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-// export { default as Button} from './components/Botton';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/* 组件库打包-start */
+export { default as Button} from './components/Botton';
+/* 组件库打包-end */
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/* 本地测试-start */
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+/* 本地测试-end */
